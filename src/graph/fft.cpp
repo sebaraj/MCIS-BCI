08300d431a8848a38150b24d53508320af33e40b
Bryan SebaRaj <bryanwsebaraj@gmail.com>
2025-10-04 00:21:53 -0400

    fix fft

--- src/graph/fft.cpp
+++ src/graph/fft.cpp
@@ -29,13 +29,21 @@ std::expected<Graph, mcis::GraphError> Graph::create_fft_graph_from_dimensions(
     }
 
     Graph graph;
-    graph.reserve_nodes(n * (static_cast<int>(log2(n)) + 1));
+    int stages = static_cast<int>(log2(n));
+    graph.reserve_nodes(n + n * stages + n);
 
     for (int i = 0; i < n; ++i) {
         graph.add_node("x_" + std::to_string(i));
     }
 
-    for (int stage = 1; stage <= static_cast<int>(log2(n)); ++stage) {
+    for (int stage = 1; stage <= stages; ++stage) {
+        for (int i = 0; i < n; ++i) {
+            graph.add_node("s" + std::to_string(stage) + "_"
+                           + std::to_string(i));
+        }
+    }
+
+    for (int stage = 1; stage <= stages; ++stage) {
         int num_butterflies = 1 << (stage - 1);
         int butterfly_size = n / num_butterflies;
         int half_size = butterfly_size / 2;
@@ -59,9 +67,6 @@ std::expected<Graph, mcis::GraphError> Graph::create_fft_graph_from_dimensions(
                 std::string bottom_node_out = "s" + std::to_string(stage) + "_"
                                               + std::to_string(bottom_idx);
 
-                graph.add_node(top_node_out);
-                graph.add_node(bottom_node_out);
-
                 graph.add_edge(top_node_in, top_node_out, 0);
                 graph.add_edge(bottom_node_in, top_node_out, 0);
                 graph.add_edge(top_node_in, bottom_node_out, 0);
@@ -71,11 +76,10 @@ std::expected<Graph, mcis::GraphError> Graph::create_fft_graph_from_dimensions(
     }
 
     for (int i = 0; i < n; ++i) {
-        std::string last_stage_node
-            = "s" + std::to_string(static_cast<int>(log2(n))) + "_"
-              + std::to_string(i);
         std::string output_node = "X_" + std::to_string(i);
         graph.add_node(output_node);
+        std::string last_stage_node
+            = "s" + std::to_string(stages) + "_" + std::to_string(i);
         graph.add_edge(last_stage_node, output_node, 0);
     }
 
